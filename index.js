5d3e817baf1ef86ff0a4fdccb442430091c47688
Patrick <ruomungai@gmail.com>
2022-06-08 22:32:43 +0300

    commit tree d86ddb34295e3e1b0d17b30ee06b0c0658f45742

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ function writeCards(names, event) {
     }
     return messages;
   }
-  
-  
-  
-  
   function countDown(countdown) {
-  //declare and instantiate your variable
    var countdown = 10;
-  
-  //set up your loop that will run while your variable is greater than zero
   while (countdown >= 0) {
-     //log timer to the console for debugging purposes
      console.log(countdown);
      countdown--;
-     //reduce the count of timer by one over each iteration
     
   }
   }
